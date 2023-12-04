a3d73f40b911b3f007facb4de7b557e1a51eccea
ozgur <oktayozgur94@gmail.com>
2023-12-04 18:56:46 +0300

    code base is ready

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const books = require('./data');
+
+app.use(cors());
+
+app.get('/books', (req, res) => {
+  res.json(books);
+});
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
