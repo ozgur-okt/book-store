d0c4102d8e429094398198b74a1f5bf8f92a6cee
ozgur <oktayozgur94@gmail.com>
2023-12-07 11:26:27 +0300

    cart item responsive design

--- client/src/components/CartItem.jsx
+++ client/src/components/CartItem.jsx
@@ -4,17 +4,19 @@ import styles from '../styles/components/CartItem.module.scss'
 
 const CartItem = ({ item }) => {
   return (
-    <Link to={`/book/${item.book.id}`} className={styles.cartItem}>
-      <div className={styles.image}>
-        <img src={item.book.image} alt={item.book.title} />
-      </div>
-      <div className={styles.info}>
-        <h2 className={styles.bookTitle}>{item.book.title}</h2>
-        <p className={styles.bookAuthor}>{item.book.author}</p>
-        <p className={styles.bookPrice}>${item.book.price}</p>
-      </div>
+    <div to={`/book/${item.book.id}`} className={styles.cartItem}>
+      <Link to={`/book/${item.book.id}`} className={styles.link}>
+        <div className={styles.image}>
+          <img src={item.book.image} alt={item.book.title} />
+        </div>
+        <div className={styles.info}>
+          <h2 className={styles.bookTitle}>{item.book.title}</h2>
+          <p className={styles.bookAuthor}>{item.book.author}</p>
+          <p className={styles.bookPrice}>${item.book.price}</p>
+        </div>
+      </Link>
       <ManageCart book={item.book} />
-    </Link>
+    </div>
   )
 }
 
